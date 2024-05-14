37c4162646bda5e0dbd6814eaee72f33740413d0
Hau <163385034+HauHyHung@users.noreply.github.com>
2024-05-14 08:55:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bảo Trân';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc công chúa ngày mới vui vẻ';
+const titleCard = 'Gửi công chúa';
+const contentCard = 'Chưa kịp Apply CV thì công chúa về lại vương quốc rồi, tối thần chuẩn bị kỹ lắm lắm lun. Công chúa đi đường cẩn thận nhá, về tới Long Xuyên thì có binh lính bảo vệ rồi!!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'cute.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
